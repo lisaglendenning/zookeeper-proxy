21cedff0a21b68497e861f82b0a16ec244069883
Lisa Glendenning <lisa@glendenning.net>
2013-04-12 14:26:44 -0700

    packaging tweaks

---
+++ src/main/bin/zookeeper-proxy.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+# Author: Lisa Glendenning <lglenden@cs.washington.edu>
+
+: ${ZOOKEEPER_PROXY_PREFIX=$(dirname $0)/..}
+: ${ZOOKEEPER_PROXY_LIB=${ZOOKEEPER_PROXY_PREFIX}/lib}
+: ${ZOOKEEPER_PROXY_ETC=${ZOOKEEPER_PROXY_PREFIX}/etc}
+: ${ZOOKEEPER_PROXY_LOG=${ZOOKEEPER_PROXY_PREFIX}/log}
+
+: ${JAVA=java}
+: ${JAVA_CLASSPATH="${ZOOKEEPER_PROXY_LIB}/*"}
+: ${JAVA_MAIN="org.apache.zookeeper.proxy.netty.NettyProxyMain"}
+
+: ${ZOOKEEPER_PROXY_ARGS=""}
+
+if [ ! -z ${ZOOKEEPER_PROXY_LOG} ]
+then
+if [ -f ${ZOOKEEPER_PROXY_ETC}/log4j2-test.xml ]
+then
+    ZOOKEEPER_PROXY_LOG="-Dlog4j.configurationFile=${ZOOKEEPER_PROXY_ETC}/log4j2-test.xml"
+elif [ -f ${ZOOKEEPER_PROXY_ETC}/log4j2.xml ]
+then
+    ZOOKEEPER_PROXY_LOG="-Dlog4j.configurationFile=${ZOOKEEPER_PROXY_ETC}/log4j2.xml"
+fi
+fi
+
+: ${JAVA_ARGS="-classpath \"${JAVA_CLASSPATH}\" ${ZOOKEEPER_PROXY_LOG}"}
+
+COMMAND="${JAVA} ${JAVA_ARGS} ${JAVA_MAIN} ${ZOOKEEPER_PROXY_ARGS}"
+
+/bin/bash -c "${COMMAND}"
