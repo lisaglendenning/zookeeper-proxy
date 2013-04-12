c6cc15a9f32192594a9d952351e272f0060a2ecc
Lisa Glendenning <lisa@glendenning.net>
2013-04-12 14:58:40 -0700

    changed from bash to eval

--- src/main/bin/zookeeper-proxy.sh
+++ src/main/bin/zookeeper-proxy.sh
@@ -9,22 +9,25 @@
 : ${JAVA=java}
 : ${JAVA_CLASSPATH="${ZOOKEEPER_PROXY_LIB}/*"}
 : ${JAVA_MAIN="org.apache.zookeeper.proxy.netty.NettyProxyMain"}
-
-: ${ZOOKEEPER_PROXY_ARGS=""}
+: ${JAVA_MAIN_ARGS=""}
 
 if [ ! -z ${ZOOKEEPER_PROXY_LOG} ]
 then
-if [ -f ${ZOOKEEPER_PROXY_ETC}/log4j2-test.xml ]
-then
-    ZOOKEEPER_PROXY_LOG="-Dlog4j.configurationFile=${ZOOKEEPER_PROXY_ETC}/log4j2-test.xml"
-elif [ -f ${ZOOKEEPER_PROXY_ETC}/log4j2.xml ]
-then
-    ZOOKEEPER_PROXY_LOG="-Dlog4j.configurationFile=${ZOOKEEPER_PROXY_ETC}/log4j2.xml"
+	if [ -f ${ZOOKEEPER_PROXY_ETC}/log4j2-test.xml ]
+	then
+	    ZOOKEEPER_PROXY_LOG="-Dlog4j.configurationFile=${ZOOKEEPER_PROXY_ETC}/log4j2-test.xml"
+	elif [ -f ${ZOOKEEPER_PROXY_ETC}/log4j2.xml ]
+	then
+	    ZOOKEEPER_PROXY_LOG="-Dlog4j.configurationFile=${ZOOKEEPER_PROXY_ETC}/log4j2.xml"
+	else
+		ZOOKEEPER_PROXY_LOG=""
+	fi
 fi
-fi
+
+# this isn't an ideal way to run the command, but it's a bit tricky
+# otherwise to avoid expanding the * in the classpath
 
 : ${JAVA_ARGS="-classpath \"${JAVA_CLASSPATH}\" ${ZOOKEEPER_PROXY_LOG}"}
 
-COMMAND="${JAVA} ${JAVA_ARGS} ${JAVA_MAIN} ${ZOOKEEPER_PROXY_ARGS}"
-
-/bin/bash -c "${COMMAND}"
+COMMAND="${JAVA} ${JAVA_ARGS} ${JAVA_MAIN} ${JAVA_MAIN_ARGS}"
+eval "${COMMAND}"
