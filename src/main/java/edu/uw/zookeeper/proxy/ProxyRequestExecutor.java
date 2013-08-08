4fee30a0cbd37129ecba5078d6655cb7f2d6ee07
Lisa Glendenning <lisa@glendenning.net>
2013-08-07 17:01:54 -0700

    update

--- src/main/java/edu/uw/zookeeper/proxy/ProxyRequestExecutor.java
+++ src/main/java/edu/uw/zookeeper/proxy/ProxyRequestExecutor.java
@@ -9,10 +9,9 @@ import edu.uw.zookeeper.net.Connection;
 import edu.uw.zookeeper.protocol.Message;
 import edu.uw.zookeeper.protocol.SessionOperation;
 import edu.uw.zookeeper.protocol.client.ClientConnectionExecutor;
-import edu.uw.zookeeper.protocol.proto.Records;
 
 public class ProxyRequestExecutor<C extends Connection<? super Message.ClientSession>> 
-        implements TaskExecutor<SessionOperation.Request<Records.Request>, Message.ServerResponse<Records.Response>> {
+        implements TaskExecutor<SessionOperation.Request<?>, Message.ServerResponse<?>> {
 
     public static <C extends Connection<? super Message.ClientSession>> ProxyRequestExecutor<C> newInstance(
             Map<Long, ClientConnectionExecutor<C>> clients) {
@@ -26,8 +25,8 @@ public class ProxyRequestExecutor<C extends Connection<? super Message.ClientSes
     }
     
     @Override
-    public ListenableFuture<Message.ServerResponse<Records.Response>> submit(
-            SessionOperation.Request<Records.Request> request) {
+    public ListenableFuture<Message.ServerResponse<?>> submit(
+            SessionOperation.Request<?> request) {
         Long sessionId = request.getSessionId();
         ClientConnectionExecutor<C> client = clients.get(sessionId);
         return client.submit(request);
